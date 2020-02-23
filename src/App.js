8395988d5f7bb757b8e32b766d53cb62fdfb2e56
Joberth Rogers <joberth.rogers18@gmail.com>
2020-02-22 20:53:03 -0400

    Added shared Navbar
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes';
+import Navbar from './shared/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes />
     </div>
   );
---
+++ src/shared/NavBar/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import EcoIcon from '@material-ui/icons/Eco';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appbar: {
+    background: 'linear-gradient(120deg, #09523B 2%, #11A558 100%)', 
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <EcoIcon />
+          <Typography variant="h6" className={classes.title}>
+            FitologiaAdmin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
